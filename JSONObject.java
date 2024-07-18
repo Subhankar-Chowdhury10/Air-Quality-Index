62bda4a0b31481d82dc78002bcde40ca6909cf65
Subhankar Chowdhury <128142425+Subhankar-Chowdhury10@users.noreply.github.com>
2024-07-19 01:14:38 +0530

    JSONObject.java

---
+++ JSONObject.java
@@ -0,0 +1,17 @@
+public class JSONObject {
+
+    public JSONObject(String body) {
+        // TODO Auto-generated constructor stub
+    }
+
+    public JSONArray getJSONArray(String string) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getJSONArray'");
+    }
+
+    public String getString(String string) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getString'");
+    }
+
+}
